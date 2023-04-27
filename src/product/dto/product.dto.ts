675cade36d3ea42a4a4868a0a606cb3301a352f7
JohanaBaez <johanarabaez617@gmail.com>
2023-04-26 22:26:01 -0600

    terminado

---
+++ src/product/dto/product.dto.ts
@@ -0,0 +1,29 @@
+import { IsArray, IsNotEmpty, IsNumber, IsOptional, IsString, MinLength } from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(20)
+  tablet: string;
+
+  @IsString()
+  @IsNotEmpty()
+  marca: string;
+
+  @IsString()
+  @IsNotEmpty()
+  color: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  tamaño: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  price: number;
+
+  @IsString({each:true})
+  @IsArray()
+  @IsOptional()
+  images?: string[];
+}
